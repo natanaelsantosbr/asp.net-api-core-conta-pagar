67c1636b0fec1381af7846e9185c1ad63229a575
Natanael Santos <natanaelsantosbr@gmail.com>
2021-01-18 23:49:14 -0300

    Criado a interface de Contas a Pagar

---
+++ Natanael/Natanael.Dominio/ContasPagar/IRepositorioDeContasPagar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Natanael.Dominio.ContasPagar
+{
+    public interface IRepositorioDeContasPagar
+    {
+        List<ContaPagar> Listar();
+
+        void Cadastrar(ContaPagar contaPagar);
+    }
+}
