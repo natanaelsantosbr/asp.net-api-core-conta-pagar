707cc38483cfc05c0aae21f9f9ccfa884a4ba4ee
Natanael Santos <natanaelsantosbr@gmail.com>
2021-01-19 02:15:30 -0300

    - Adicionar a referencia (Aplicacao de Consumo) - Criar a controller de ContasPagar - Criar as views - Criar o layout

---
+++ Natanael/Natanael.Web/Controllers/ContasPagarController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Natanael.Aplicacao.Web.ConsumirContasPagar.Modelos;
+using Natanael.Aplicacao.Web.ConsumirContasPagar.Servicos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Natanael.Web.Controllers
+{
+    public class ContasPagarController : Controller
+    {
+        private IServicoDeConsumoDeContasPagar _servicoDeConsumoDeContasPagar;
+
+        public ContasPagarController(IServicoDeConsumoDeContasPagar servicoDeConsumoDeContasPagar)
+        {
+            this._servicoDeConsumoDeContasPagar = servicoDeConsumoDeContasPagar;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var retorno = await this._servicoDeConsumoDeContasPagar.Listar();
+
+            return View(retorno);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Cadastrar(ModeloDeConsumoDeCadastroDeContaPagar modelo)
+        {
+            var retorno = await this._servicoDeConsumoDeContasPagar.Cadastrar(modelo);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Natanael/Natanael.Web/wwwroot/js/paginas/contaspagar/cadastrarContasPagar.js
@@ -0,0 +1,36 @@
+﻿jQuery(function ($) {
+    let formCadastro = $('#formCadastrarContaPagar');
+    if (formCadastro.length) {
+        formCadastro.validate({
+            errorClass: "text-danger",
+            rules: {
+                Nome: {
+                    required: true
+                },
+                Valor: {
+                    required: true
+                },
+                DataDeVencimento: {
+                    required: true
+                },
+                DataDePagamento: {
+                    required: true
+                },
+            },
+            messages: {
+                Nome: {
+                    required: 'Preencha o campo Nome para continuar'
+                },
+                Valor: {
+                    required: 'Preencha o campo Valor para continuar'
+                },
+                DataDeVencimento: {
+                    required: 'Preencha o campo Data de Vencimento para continuar'
+                },
+                DataDePagamento: {
+                    required: 'Preencha o campo Data de Pagamento para continuar'
+                }
+            }
+        });
+    }
+})
