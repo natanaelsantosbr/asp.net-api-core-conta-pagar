b7e4084d3692b6d2ec8b65e1fa4b95feb6dbd096
Natanael Santos <natanaelsantosbr@gmail.com>
2021-01-18 23:48:08 -0300

    Criado a classe ContaPagar

---
+++ Natanael/Natanael.Dominio/ContasPagar/ContaPagar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Natanael.Dominio.ContasPagar
+{
+    public class ContaPagar : Entidade
+    {
+        public ContaPagar()
+        {
+
+        }
+
+        public ContaPagar(string nome, double valor, DateTime dataDeVencimento, DateTime dataDePagamento) : this()
+        {
+            this.Nome = nome;
+            this.Valor = valor;
+            this.DataDeVencimento = dataDeVencimento;
+            this.DataDePagamento = dataDePagamento;
+
+            this.ProcessarFinanceiro();
+
+
+        }
+
+        public string Nome { get; private set; }
+        public double Valor { get; private set; }
+        public DateTime DataDeVencimento { get; private set; }
+        public DateTime DataDePagamento { get; private set; }
+        public int DiasDeAtraso { get; private set; }
+        public double PercentualDaMulta { get; private set; }
+        public double ValorDaMulta { get; private set; }
+        public double PercentualDosJuros { get; private set; }
+        public double ValorDosJuros { get; private set; }
+
+        private void ProcessarFinanceiro()
+        {
+            this.DiasDeAtraso = 10;
+            this.PercentualDaMulta = 10.0;
+            this.ValorDaMulta = 15.00;
+            this.PercentualDosJuros = 5.00;
+            this.ValorDosJuros = 10.0;
+        }
+    }
+}
---
+++ Natanael/Natanael.Dominio/Entidade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Natanael.Dominio
+{
+    public class Entidade
+    {
+        public Entidade()
+        {
+            DataDoCadastro = DateTime.Now;
+        }
+        public int Id { get; private set; }
+
+        public DateTime DataDoCadastro { get; private set; }
+    }
+}
